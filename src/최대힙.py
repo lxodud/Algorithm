23ec71bba81acecb6a40034eeb5e00f41aebba80
Hamo <85005933+lxodud@users.noreply.github.com>
2025-05-14 18:18:48 +0900

    Create 최대힙.py

---
+++ src/최대힙.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/11279
+
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+  number = int(input())
+  
+  if number == 0:
+    if heap:
+      print(-heappop(heap))
+    else:
+      print(0)
+  else:
+    heappush(heap, number * -1)
